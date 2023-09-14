7bece3b13694097a363500995d80c1ec6625dae8
mustafairshaidd <mustafa.irshaid.113@gmail.com>
2023-09-14 10:31:22 +0300

    Implemented Post Button in NavBar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import NavItem from "./NavItem";
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import { Box, Button } from "@mui/material";
 const Navbar = () => {
   return (
     <>
       <NavItem  icon={<HomeOutlinedIcon/>} title={"Home"}/>
+      <NavItem  icon={<HomeOutlinedIcon/>} title={"Home"}/>
+      <NavItem  icon={<HomeOutlinedIcon/>} title={"Home"}/>
+      <NavItem  icon={<HomeOutlinedIcon/>} title={"Home"}/>
+      <NavItem  icon={<HomeOutlinedIcon/>} title={"Home"}/>
+      <Box width={"100%"} justifyContent={"center"} alignItems={"center"} display={"flex"}>
+          <Button variant={"contained"} sx={{borderRadius:"20px",padding:"7px 0px",width:"90%",marginTop:"20px"}}>Post</Button>
+      </Box>
+      
     </>
   );
 };
